f5a3a2aa12fb5f7ad11a6b1c29f87e1171c50663
Pijus Serapinas <pijus.serapinas@jbgvilnius.lt>
2022-02-22 09:17:04 +0200

    Added create project

--- controllers/project.go
+++ controllers/project.go
@@ -8,18 +8,41 @@ import (
 	"github.com/noclueps/fablab/utils"
 )
 
+type createProjectRequest struct {
+	Title string `json:"title"`
+	Description string `json:"description"`
+	WhoWrokedOn string `json:"whoWorkedOn"`
+	Supervisor string `json:"supervisor"`
+	Images pq.StringArray `gorm:"type:string[]" json:"images"`
+}
+
 func CreateProject(c *fiber.Ctx) error {
 	tokenStr := c.Cookies("jwt")
-	_, err := utils.ExtractClaims(tokenStr)
+	claims, err := utils.ExtractClaims(tokenStr)
+	db := database.Database.DB
+	req := new(createProjectRequest)
 
 	if !err {
 		return fiber.NewError(fiber.StatusBadRequest, "Unauthorized")
 	}
 
-	c.JSON(models.Project{
-		Images: []string{"something", "something_else"},
-	})
+	var user models.User
+	db.Where("ID = ?", claims["user_id"] ).First(&user)
 	
+	if user.ID == 0 {
+		return fiber.NewError(fiber.StatusBadRequest, "user not found!")
+	}
+
+	var project models.Project{}
+	project.Title = req.Title
+	project.Description = req.Description
+	project.WhoWrokedOn = req.WhoWrokedOn
+	project.Supervisor = req.Supervisor
+	project.Images = req.Images
+	prject.Author = user.Name
+
+	c.JSON(project)
+
 	return nil
 }
 
